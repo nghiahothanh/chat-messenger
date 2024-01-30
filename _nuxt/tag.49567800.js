6412571455b71f458218d1a5145c02b4d6f81382
nghiahothanh <96303069+nghiahothanh@users.noreply.github.com>
2024-01-31 02:11:40 +0700

    Updates

---
+++ _nuxt/tag.49567800.js
@@ -0,0 +1,1 @@
+import{x as t}from"./entry.48205076.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- _nuxt/tag.60ff9bab.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./entry.110dc7e9.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
