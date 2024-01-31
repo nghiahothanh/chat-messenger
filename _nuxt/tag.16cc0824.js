32bc583a747ef7fde3026bdcabb70a52ec01e87e
nghiahothanh <96303069+nghiahothanh@users.noreply.github.com>
2024-01-31 11:04:41 +0700

    Updates

---
+++ _nuxt/tag.16cc0824.js
@@ -0,0 +1,1 @@
+import{x as t}from"./entry.38e0bbdf.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- _nuxt/tag.cf2f032a.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./entry.c2ff51a9.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
