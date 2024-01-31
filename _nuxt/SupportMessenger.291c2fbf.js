2273048b6e130cf991feacbdcae2a8734875fac2
nghiahothanh <96303069+nghiahothanh@users.noreply.github.com>
2024-01-31 11:25:56 +0700

    Updates

---
+++ _nuxt/SupportMessenger.291c2fbf.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as _,b as o,w as c,d as e}from"./entry.ffe928d2.js";import{V as r}from"./index.8af4e87f.js";const n={},a={class:"support-messenger-trigger"};function p(d,i){return t(),_("div",a,[o(r,{size:"16",class:"pr-1"},{default:c(()=>[e(" mdi-help-circle-outline ")]),_:1}),e(" Help ")])}const u=s(n,[["render",p],["__scopeId","data-v-5be583e1"]]);export{u as _};
